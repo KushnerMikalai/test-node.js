00c58efc90e3661ad0c75ed7bd0904ad29b5e03f
Nikolay Kushner <nickolaikushner@gmail.com>
2019-05-21 15:17:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const routes = require('./routes');
 
-const server = http.createServer(routes.handler);
+const serverolo = http.createServer(routes.handler);
 
 server.listen(9990);
