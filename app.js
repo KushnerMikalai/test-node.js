c96e3d7ca5ac04aa7168f4fb558b35b270659898
KushnerMikalai <nickolaikushner@gmail.com>
2019-05-22 17:19:04 +0300

    +

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-const server = http.createServer(app);
 
+app.use((req, res, next) => {
+  console.log('In the moddleware!')
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send(`
+    <h1>Ololo</h1>
+    <div>ololo2</div>
+  `)
+});
+
+const server = http.createServer(app);
 server.listen(9990);
