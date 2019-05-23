b78029c437e7dd80a45a4a5233ac38854bf12461
Kushner Mikalai <nickolaikushner@gmail.com>
2019-05-23 22:17:13 +0300

    feat: add HTML

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(adminRoutes);
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send(`<div>404 Page Not Found</div>`);
+});
 
 app.listen(9990);
