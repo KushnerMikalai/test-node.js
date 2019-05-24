6783c88bc177e32b7dd4883fc8bf914264157a38
KushnerMikalai <nickolaikushner@gmail.com>
2019-05-24 11:05:04 +0300

    feat: add html, css, and path feature

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require('express');
+const rootDir = require('../util/path');
 
 const router =  express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.send(`<form action="/admin/add-product" method="POST"><input name="title" type="text"><button type="submit">Add product</button></form>`)
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
 router.post('/add-product', (req, res) => {
