0631957166b0a6a2fc5e9ee1a7be616bed6b9f51
Kushner Mikalai <nickolaikushner@gmail.com>
2019-05-25 18:10:27 +0300

    feat: pug

--- app.js
+++ app.js
@@ -2,18 +2,51 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+/**
+ * app express
+ */
 const app = express();
-const adminRoutes = require('./routes/admin');
+
+/**
+ * template engine
+ */
+app.set('view engine', 'pug');
+
+/**
+ * dir for views template engine
+ */
+app.set('views', 'views');
+
+/**
+ * routes
+ */
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+/**
+ * parse enter data
+ */
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public'))); // add
 
-app.use('/admin', adminRoutes);
+/**
+ * set static dir for all files (img, css, js)
+ */
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * use routes
+ */
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
+/**
+ * use 404 page
+ */
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
+/**
+ * listen server
+ */
 app.listen(9990);
