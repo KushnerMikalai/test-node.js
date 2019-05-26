6f58fbf42775cf6dabbc4ab9143cd0ba6a4fb754
Nikolay Kushner <nickolaikushner@gmail.com>
2019-05-26 16:44:43 +0300

    Update shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const express = require('express');
+const expre = require('express');
 const rootDir = require('../util/path');
 const adminData = require('./admin');
 
