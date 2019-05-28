4721ade7ba3cbf1cd8d885ffd3849975cff4bd61
KushnerMikalai <nickolaikushner@gmail.com>
2019-05-28 10:43:01 +0300

    feat: pug full

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(shopRoutes);
  * use 404 page
  */
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', { pageTitle: 'Page Not Found'})
 });
 
 /**
