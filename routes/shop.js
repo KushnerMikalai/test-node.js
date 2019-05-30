d6a7a3f7b31ece47282d06fa024dd7e926ac3d2f
KushnerMikalai <nickolaikushner@gmail.com>
2019-05-30 23:41:52 +0300

    feat: add files to views

--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,13 @@ const express = require('express');
 const productsController = require('../controllers/ProductsController');
 const router =  express.Router();
 
-router.get('/', productsController.getProducts);
+
+router.get('/', (req, res, next) => {
+  res.render('shop/index', {
+    pageTitle: 'Panda Words',
+    path: '/'
+  })
+});
+// router.get('/', productsController.getProducts);
 
 module.exports = router;
