80f89020ce11fe8bd69c6cd038457808244a0304
KushnerMikalai <nickolaikushner@gmail.com>
2019-06-11 23:24:54 +0300

    feat: add next layout

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,37 @@
-import Header from '../components/Header'
+import Layout from '../components/layout/default'
 import Head from 'next/head'
+import Link from 'next/link';
 
-const Index = () => (
-  <div>
+const Index = props => (
+  <Layout>
     <Head>
       <title>Nooooo!</title>
     </Head>
-    <Header />
-    <div>Hello Next.js</div>
-  </div>
+
+    <h1>News</h1>
+    <ul>
+      {props.posts.map(post => (
+        <li key={post.id}>
+          <Link as={`/p/${post.id}`} href={`/post?id=${post.id}`}>
+            <a>{post.title}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+    <p>Index</p>
+  </Layout>
 );
+
+Index.getInitialProps = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    posts: data.map(post => post)
+  };
+};
+
 
 export default Index
